8b9083b910d1053b2c2ed2fdb3003ac1e5bfe868
Matt Burke <spraints@gmail.com>
2010-02-22 14:49:50 -0500

    Got the blue_ridge generator to work.

--- lib/generators/blue_ridge/blue_ridge_generator.rb
+++ lib/generators/blue_ridge/blue_ridge_generator.rb
@@ -1,17 +1,17 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../lib/blue_ridge')
+require 'rails/generators'
 
-class BlueRidgeGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      base_dir = BlueRidge.javascript_spec_dir
-      
-      m.directory base_dir
-      m.file 'application_spec.js', "#{base_dir}/application_spec.js"
-      m.file 'spec_helper.js',      "#{base_dir}/spec_helper.js"
-      
-      m.directory "#{base_dir}/fixtures"
-      m.file 'application.html', "#{base_dir}/fixtures/application.html"
-      m.file 'screw.css',        "#{base_dir}/fixtures/screw.css"
-    end
+require File.expand_path(File.dirname(__FILE__) + '/../../blue_ridge')
+
+class BlueRidgeGenerator < Rails::Generators::Base
+  def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
+  end
+
+  def install
+    base_dir = BlueRidge.javascript_spec_dir
+    template 'application_spec.js', "#{base_dir}/application_spec.js"
+    template 'spec_helper.js',      "#{base_dir}/spec_helper.js"
+    template 'application.html',    "#{base_dir}/fixtures/application.html"
+    template 'screw.css',           "#{base_dir}/fixtures/screw.css"
   end
 end
